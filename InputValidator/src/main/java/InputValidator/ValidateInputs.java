594d217832fa1eb501d462549617740b0734edd5
Jordan Terry <jordan@jordanterry.co.uk>
2016-07-24 19:11:50 +0100

    Create a list of InputValidators from 1..n Inputs

--- InputValidator/src/main/java/InputValidator/ValidateInputs.java
+++ InputValidator/src/main/java/InputValidator/ValidateInputs.java
@@ -1,6 +1,11 @@
 package InputValidator;
 
+import android.support.annotation.NonNull;
 import android.widget.TextView;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A factory to create an {@link InputValidator}
@@ -9,13 +14,32 @@ public class ValidateInputs {
 
     /**
      * Create a validated input passed into the method.
-     * @param input The input to be validated
+     * @param input the input to be validated
      * @param <T> a generic object that extends TextView
-     * @return Validated Input object
+     * @return A validated input object
      */
-    public static <T extends TextView> InputValidator validate(T input) {
+    public static <T extends TextView> InputValidator validate(@NonNull T input) {
         return new DefaultInputValidator<>(input);
     }
+
+
+    /**
+     * Create a List of validated inputs by passing 1..n inputs
+     * @param inputs 1..n inputs
+     * @param <T> a generic object that extends TextView
+     * @return a List of validated input objects.
+     */
+    public static <T extends TextView> List<InputValidator> validate(@NonNull  T... inputs) {
+        if (inputs.length == 0) {
+            throw new InvalidParameterException();
+        }
+        List<InputValidator> validators = new ArrayList<>();
+        for (int i = 0; i < inputs.length; i++) {
+            validators.add(new DefaultInputValidator(inputs[i]));
+        }
+        return validators;
+    }
+
 
 
 }
